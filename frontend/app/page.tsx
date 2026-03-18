15509536291b4999a0f6d6e6e4d23382dfc7a970
AtwolfOG <ayokunletestimony702@gmail.com>
2026-03-18 14:21:09 +0100

    switched directory

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/hero";
 import Image from "next/image";
 import Problems from "@/components/problems";
+import Features from "@/components/features";
 export default function Home() {
   return (
     <div>
       <Hero />
       <Problems />
+      <Features />
     </div>
   );
 }
--- frontend/components/problems.tsx
+++ frontend/components/problems.tsx
@@ -3,7 +3,7 @@ export default function Problems() {
         <div className="problem h-[700px] w-full">
             <div className="flex flex-col items-center justify-center h-full">
                 <h2>Technical interviews are broken</h2>
-                <p className="w-[60ch] text-center my-4 text-(--text-secondary)">Traditional interviews rely on screen sharing, scattered tools, and poorly structured coding challenges. Interviewers struggle to evaluate candidates effectively, and candidates often face confusing setups</p>
+                <p className="w-[60ch] text-center my-4 text-(--text-secondary) ">Traditional interviews rely on screen sharing, scattered tools, and poorly structured coding challenges. Interviewers struggle to evaluate candidates effectively, and candidates often face confusing setups</p>
             </div>
         </div>
     );
